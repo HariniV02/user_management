b28bcb5e902503d555fc0e6dc5175ea943303e1d
HariniV02 <vharinirao@gmail.com>
2024-12-17 09:54:04 -0500

    editing password code

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -55,22 +55,22 @@ class UserBase(BaseModel):
 
 class UserCreate(UserBase):
     email: EmailStr = Field(..., example="john.doe@example.com")
-    password: str = Field(..., min_length=8, example="Secure*1234")
+    password: str = Field(..., example="Secure*1234")
 
-    # Password validation
+    # Updated Password Validation
     @validator('password')
-    def validate_password(cls, va):
-        if len(va) < 8:
-            raise ValueError("Password must be at least 8 characters long.")
-        if not re.search(r"[A-Z]", va):
-            raise ValueError("Password must contain at least one uppercase letter.")
-        if not re.search(r"[a-z]", va):
-            raise ValueError("Password must contain at least one lowercase letter.")
-        if not re.search(r"\d", va):
-            raise ValueError("Password must contain at least one digit.")
-        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", va):
-            raise ValueError("Password must contain at least one special character.")
-        return va
+    def validate_password(cls, password):
+        if len(password) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        if not any(char.isdigit() for char in password):
+            raise ValueError("Password must include at least one digit")
+        if not any(char.isupper() for char in password):
+            raise ValueError("Password must include at least one uppercase letter")
+        if not any(char.islower() for char in password):
+            raise ValueError("Password must include at least one lowercase letter")
+        if not any(char in "!@#$%^&*()-_=+[{]}\|;:'\",<.>/?`~" for char in password):
+            raise ValueError("Password must include at least one special character")
+        return password
 
 
 class UserUpdate(UserBase):
